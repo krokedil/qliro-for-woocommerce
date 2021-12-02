96c3741dc8124b114a1517243e19504d4eb20ea1
Miodrag Stojanovic <mio.sto@yandex.com>
2021-12-02 13:21:03 +0100

    add qliro gateway class

---
+++ classes/class-qliro-one-gateway.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Class file for Qliro_One_Gateway class.
+ *
+ * @package Qliro_One_For_WooCommerce/Classes
+ */
+
+/**
+ * Class Qliro_One_Gateway
+ */
+class Qliro_One_Gateway extends WC_Payment_Gateway {
+
+	/**
+	 * Class constructor.
+	 */
+	public function __construct() {
+		$this->id                 = 'qliro_one';
+		$this->method_title       = __( 'Qliro One', 'qliro-one-for-woocommerce' );
+		$this->method_description = __( 'Qliro One replaces the standard WooCommerce checkout page.', 'qliro-one-for-woocommerce' );
+		$this->supports           = apply_filters(
+			'qliro_one_gateway_supports',
+			array(
+				'products',
+			)
+		);
+		$this->has_fields         = false;
+		$this->init_form_fields();
+		$this->init_settings();
+		$this->title       = $this->get_option( 'title' );
+		$this->description = $this->get_option( 'description' );
+		$this->enabled     = $this->get_option( 'enabled' );
+		$this->testmode    = 'yes' === $this->get_option( 'testmode' );
+		$this->logging     = 'yes' === $this->get_option( 'logging' );
+		add_action(
+			'woocommerce_update_options_payment_gateways_qliro_one',
+			array(
+				$this,
+				'process_admin_options',
+			)
+		);
+	}
+
+
+	/**
+	 * Initialise settings fields.
+	 */
+	public function init_form_fields() {
+		$this->form_fields = Qliro_One_Fields::fields();
+	}
+
+	/**
+	 * Checks if method should be available.
+	 *
+	 * @return boolean
+	 */
+	public function is_available() {
+		return ! ( 'yes' !== $this->enabled );
+	}
+}
