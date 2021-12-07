338cc4ec9aa30141eb6d4c14e7e305456e60c51f
Miodrag Stojanovic <mio.sto@yandex.com>
2021-12-07 11:56:15 +0100

    add calculate_auth

--- classes/requests/class-qliro-one-request-post.php
+++ classes/requests/class-qliro-one-request-post.php
@@ -21,7 +21,17 @@ abstract class Qliro_One_Request_Post extends Qliro_One_Request {
 	 */
 	public function __construct( $arguments = array() ) {
 		parent::__construct( $arguments );
-		$this->method         = 'POST';
+		$this->method = 'POST';
+	}
+
+	/**
+	 * Calculates the Qliro One auth.
+	 *
+	 * @return string
+	 */
+	protected function calculate_auth() {
+		$secret = 'yes' === $this->settings['testmode'] ? 'test_api_secret' : 'api_secret';
+		return 'Qliro ' . base64_encode( hex2bin( hash( 'sha256', wp_json_encode( $this->get_body() ) . $this->settings[ $secret ] ) ) ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions -- Base64 used to calculate auth header.
 	}
 
 	/**
