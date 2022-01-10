38c5f4977baf7fc85c9d1ed2cfb1cc4af0d6e4c9
Miodrag Stojanovic <mio.sto@yandex.com>
2022-01-10 12:29:57 +0100

    update boody params
    update merchant ref

--- classes/requests/post/class-qliro-one-update-merchant-reference.php
+++ classes/requests/post/class-qliro-one-update-merchant-reference.php
@@ -38,11 +38,14 @@ class Qliro_One_Update_Merchant_Reference extends Qliro_One_Request_Post {
 	 */
 	protected function get_body() {
 		// todo change request id and order id.
+		$order_id       = $this->arguments['order_id'];
+		$order          = wc_get_order( $order_id );
+		$qliro_order_id = get_post_meta( $order_id, '_qliro_one_order_id' );
 		return array(
-			'RequestId'            => 'fd421f5f-d5cb-442b-a45e-de46dc38b586',
+			'RequestId'            => sprintf( '%04X%04X-%04X-%04X-%04X-%04X%04X%04X', random_int( 0, 65535 ), random_int( 0, 65535 ), random_int( 0, 65535 ), random_int( 16384, 20479 ), random_int( 32768, 49151 ), random_int( 0, 65535 ), random_int( 0, 65535 ), random_int( 0, 65535 ) ),
 			'MerchantApiKey'       => $this->get_qliro_key(),
-			'OrderId'              => 1215412,
-			'NewMerchantReference' => 'merchant-reference-new',
+			'OrderId'              => $qliro_order_id,
+			'NewMerchantReference' => $order->get_id(),
 		);
 	}
 }
