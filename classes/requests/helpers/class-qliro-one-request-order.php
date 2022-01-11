2e3e705a69e5bc8716596bd7919c5e879a3daa78
Miodrag Stojanovic <mio.sto@yandex.com>
2022-01-11 09:26:04 +0100

    adjust order functions for qliro
    add a function to generate GUID formatted string

--- classes/requests/helpers/class-qliro-one-request-order.php
+++ classes/requests/helpers/class-qliro-one-request-order.php
@@ -15,13 +15,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Qliro_One_Request_Order {
 
 	/**
-	 * Send sales tax as separate item (US merchants).
-	 *
-	 * @var bool
-	 */
-	public $separate_sales_tax = false;
-
-	/**
 	 * Total tax of order.
 	 *
 	 * @var int
@@ -72,7 +65,7 @@ class Qliro_One_Request_Order {
 	}
 
 	/**
-	 * Gets the formated order line.
+	 * Gets the formatted order line.
 	 *
 	 * @param WC_Order_Item_Product $order_item The WooCommerce order line item.
 	 * @return array
@@ -81,14 +74,11 @@ class Qliro_One_Request_Order {
 		$order_id = $order_item->get_order_id();
 		$order    = wc_get_order( $order_id );
 		return array(
-			'name'              => $order_item->get_name(),
-			'quantity'          => $order_item->get_quantity(),
-			'total_amount'      => $this->get_item_total_amount( $order, $order_item ),
-			'unit_price'        => $this->get_item_unit_price( $order, $order_item ),
-			'total_tax_amount'  => $this->get_item_total_tax_amount( $order, $order_item ),
-			'tax_rate'          => $this->get_order_line_tax_rate( $order, $order_item ),
-			'unit_price'        => number_format( ( $order_item->get_total() / $order_item->get_quantity() ), 2 ),
-			'unit_price_no_tax' => number_format( ( ( $order_item->get_total() - $order_item->get_total_tax() ) / $order_item->get_quantity() ), 2 ),
+			'MerchantReference'  => $order_item->get_name(),
+			'Description'        => $order_item->get_name(),
+			'Quantity'           => $order_item->get_quantity(),
+			'PricePerItemIncVat' => $this->get_item_unit_price( $order, $order_item ),
+			'PricePerItemExVat'  => $this->get_item_unit_price_no_price( $order, $order_item ),
 		);
 	}
 
@@ -100,31 +90,27 @@ class Qliro_One_Request_Order {
 	 */
 	public function get_order_line_shipping( $order ) {
 		return array(
-			'name'             => $order->get_shipping_method(),
-			'quantity'         => 1,
-			'total_amount'     => $this->get_shipping_total_amount( $order ),
-			'unit_price'       => $this->get_shipping_total_amount( $order ),
-			'total_tax_amount' => $this->get_shipping_total_tax_amount( $order ),
-			'tax_rate'         => ( '0' !== $order->get_shipping_tax() ) ? $this->get_order_line_tax_rate( $order, current( $order->get_items( 'shipping' ) ) ) : 0,
+			'MerchantReference'  => $order->get_order_number(),
+			'Description'        => $order->get_shipping_method(),
+			'Quantity'           => 1,
+			'PricePerItemIncVat' => $this->get_shipping_total_amount( $order ),
+			'PricePerItemExVat'  => $this->get_shipping_total_amount( $order ) - $order->get_shipping_tax(),
 		);
 	}
 
 	/**
-	 * Gets the formated order line fees.
+	 * Gets the formatted order line fees.
 	 *
 	 * @param WC_Order_Item_Fee $order_fee The order item fee.
 	 * @return array
 	 */
 	public function get_order_line_fees( $order_fee ) {
-		$order_id = $order_fee->get_order_id();
-		$order    = wc_get_order( $order_id );
 		return array(
-			'name'             => substr( $order_fee->get_name(), 0, 254 ),
-			'quantity'         => $order_fee->get_quantity(),
-			'total_amount'     => $this->get_fee_total_amount( $order, $order_fee ),
-			'unit_price'       => $this->get_fee_unit_price( $order_fee ),
-			'total_tax_amount' => $this->get_fee_total_tax_amount( $order, $order_fee ),
-			'tax_rate'         => ( '0' !== $order->get_total_tax() ) ? $this->get_order_line_tax_rate( $order, current( $order->get_items( 'fee' ) ) ) : 0,
+			'MerchantReference'  => $order_fee->get_id(),
+			'Description'        => $order_fee->get_name(),
+			'Quantity'           => 1,
+			'PricePerItemIncVat' => $order_fee->get_amount(),
+			'PricePerItemExVat'  => $order_fee->get_amount() - $order_fee->get_total_tax(),
 		);
 	}
 
@@ -140,10 +126,8 @@ class Qliro_One_Request_Order {
 		foreach ( $tax_items as $tax_item ) {
 			$rate_id = $tax_item->get_rate_id();
 			foreach ( $order_item->get_taxes()['total'] as $key => $value ) {
-				if ( '' !== $value ) {
-					if ( $rate_id === $key ) {
-						return round( WC_Tax::_get_tax_rate( $rate_id )['tax_rate'] * 100 );
-					}
+				if ( ( '' !== $value ) && $rate_id === $key ) {
+					return round( WC_Tax::_get_tax_rate( $rate_id )['tax_rate'] );
 				}
 			}
 		}
@@ -159,13 +143,9 @@ class Qliro_One_Request_Order {
 	 * @return int
 	 */
 	public function get_item_total_amount( $order, $order_item = false ) {
-		if ( $this->separate_sales_tax ) {
-			$item_total_amount     = number_format( ( $order_item->get_total() ) * ( 1 + ( $this->get_order_line_tax_rate( $order, $order_item ) / 10000 ) ), wc_get_price_decimals(), '.', '' ) * 100;
-			$max_order_line_amount = ( number_format( ( $order_item->get_total() + $order_item->get_total_tax() ) * 100, wc_get_price_decimals(), '.', '' ) * $order_item->get_quantity() ) * 100;
-		} else {
-			$item_total_amount     = ( number_format( $order_item->get_total(), wc_get_price_decimals(), '.', '' ) + number_format( $order_item->get_total_tax(), wc_get_price_decimals(), '.', '' ) ) * 100;
-			$max_order_line_amount = ( number_format( ( $order_item->get_total() + $order_item->get_total_tax() ) * 100, wc_get_price_decimals(), '.', '' ) * $order_item->get_quantity() ) * 100;
-		}
+
+		$item_total_amount     = ( number_format( $order_item->get_total(), wc_get_price_decimals(), '.', '' ) + number_format( $order_item->get_total_tax(), wc_get_price_decimals(), '.', '' ) );
+		$max_order_line_amount = ( number_format( ( $order_item->get_total() + $order_item->get_total_tax() ) * 100, wc_get_price_decimals(), '.', '' ) * $order_item->get_quantity() );
 		// Check so the line_total isn't greater than product price x quantity.
 		// This can happen when having price display set to 0 decimals.
 		if ( $item_total_amount > $max_order_line_amount ) {
@@ -182,12 +162,20 @@ class Qliro_One_Request_Order {
 	 * @return int
 	 */
 	public function get_item_unit_price( $order, $order_item = false ) {
-		if ( $this->separate_sales_tax ) {
-			$item_subtotal = $order_item->get_total() / $order_item->get_quantity();
-		} else {
-			$item_subtotal = ( ( number_format( $order_item->get_total(), wc_get_price_decimals(), '.', '' ) +
-			                     number_format( $order_item->get_total_tax(), wc_get_price_decimals(), '.', '' ) ) / $order_item->get_quantity() ) * 100;
-		}
+		$item_subtotal = $order_item->get_total() / $order_item->get_quantity();
+		return round( $item_subtotal );
+	}
+
+	/**
+	 * Get item price ( excluding ).
+	 *
+	 * @param WC_Order       $order The WooCommerce order.
+	 * @param boolean|object $order_item Order item.
+	 *
+	 * @return float
+	 */
+	public function get_item_unit_price_no_price( $order, $order_item = false ) {
+		$item_subtotal = ( $order_item->get_total() - $this->get_item_total_tax_amount( $order, $order_item ) ) / $order_item->get_quantity();
 		return round( $item_subtotal );
 	}
 
@@ -199,14 +187,11 @@ class Qliro_One_Request_Order {
 	 * @return int
 	 */
 	public function get_item_total_tax_amount( $order, $order_item = false ) {
-		if ( $this->separate_sales_tax ) {
-			$item_tax_amount = 0;
-		} else {
-			$item_total_amount       = $this->get_item_total_amount( $order, $order_item );
-			$item_total_exluding_tax = $item_total_amount / ( 1 + ( $this->get_order_line_tax_rate( $order, $order_item ) / 10000 ) );
-			$item_tax_amount         = $item_total_amount - $item_total_exluding_tax;
-		}
-		$this->total_tax += round( $item_tax_amount );
+
+		$item_total_amount        = $this->get_item_total_amount( $order, $order_item );
+		$item_total_excluding_tax = $item_total_amount / ( 1 + ( $this->get_order_line_tax_rate( $order, $order_item ) ) );
+		$item_tax_amount          = $item_total_amount - $item_total_excluding_tax;
+		$this->total_tax         += round( $item_tax_amount );
 		return round( $item_tax_amount );
 	}
 
@@ -217,12 +202,7 @@ class Qliro_One_Request_Order {
 	 * @return int
 	 */
 	public function get_shipping_total_amount( $order ) {
-		if ( $this->separate_sales_tax ) {
-			$shipping_amount = (int) number_format( $order->get_shipping_total() * 100, 0, '', '' );
-		} else {
-			$shipping_amount = number_format( $order->get_shipping_total() + $order->get_shipping_tax(), wc_get_price_decimals(), '.', '' ) * 100;
-		}
-		return $shipping_amount;
+		return number_format( $order->get_shipping_total() + $order->get_shipping_tax(), wc_get_price_decimals(), '.', '' );
 	}
 
 	/**
@@ -232,15 +212,11 @@ class Qliro_One_Request_Order {
 	 * @return int
 	 */
 	public function get_shipping_total_tax_amount( $order ) {
-		if ( $this->separate_sales_tax ) {
-			$shipping_tax_amount = 0;
-		} else {
-			$shipping_total_amount       = $this->get_shipping_total_amount( $order );
-			$shipping_tax_rate           = ( '0' !== $order->get_shipping_tax() ) ? $this->get_order_line_tax_rate( $order, current( $order->get_items( 'shipping' ) ) ) : 0;
-			$shipping_total_exluding_tax = $shipping_total_amount / ( 1 + ( $shipping_tax_rate / 10000 ) );
-			$shipping_tax_amount         = $shipping_total_amount - $shipping_total_exluding_tax;
-		}
-		$this->total_tax += round( $shipping_tax_amount );
+		$shipping_total_amount       = $this->get_shipping_total_amount( $order );
+		$shipping_tax_rate           = ( '0' !== $order->get_shipping_tax() ) ? $this->get_order_line_tax_rate( $order, current( $order->get_items( 'shipping' ) ) ) : 0;
+		$shipping_total_exluding_tax = $shipping_total_amount / ( 1 + ( $shipping_tax_rate ) );
+		$shipping_tax_amount         = $shipping_total_amount - $shipping_total_exluding_tax;
+		$this->total_tax            += round( $shipping_tax_amount );
 		return round( $shipping_tax_amount );
 	}
 
@@ -252,13 +228,8 @@ class Qliro_One_Request_Order {
 	 * @return int
 	 */
 	public function get_fee_total_amount( $order, $order_fee ) {
-		if ( $this->separate_sales_tax ) {
-			$fee_total_amount      = number_format( ( $order_fee->get_total() ) * ( 1 + ( $this->get_order_line_tax_rate( $order, $order_fee ) / 10000 ) ), wc_get_price_decimals(), '.', '' ) * 100;
-			$max_order_line_amount = ( number_format( ( $order_fee->get_total() + $order_fee->get_total_tax() ) * 100, wc_get_price_decimals(), '.', '' ) * $order_fee->get_quantity() ) * 100;
-		} else {
-			$fee_total_amount      = number_format( ( $order_fee->get_total() ) * ( 1 + ( $this->get_order_line_tax_rate( $order, $order_fee ) / 10000 ) ), wc_get_price_decimals(), '.', '' ) * 100;
-			$max_order_line_amount = ( number_format( ( $order_fee->get_total() + $order_fee->get_total_tax() ) * 100, wc_get_price_decimals(), '.', '' ) * $order_fee->get_quantity() ) * 100;
-		}
+		$fee_total_amount      = number_format( ( $order_fee->get_total() ) * ( 1 + ( $this->get_order_line_tax_rate( $order, $order_fee ) ) ), wc_get_price_decimals(), '.', '' );
+		$max_order_line_amount = ( number_format( ( $order_fee->get_total() + $order_fee->get_total_tax() ) * 100, wc_get_price_decimals(), '.', '' ) * $order_fee->get_quantity() );
 		// Check so the line_total isn't greater than product price x quantity.
 		// This can happen when having price display set to 0 decimals.
 		if ( $fee_total_amount > $max_order_line_amount ) {
@@ -274,12 +245,8 @@ class Qliro_One_Request_Order {
 	 * @return int
 	 */
 	public function get_fee_unit_price( $order_fee ) {
-		if ( $this->separate_sales_tax ) {
-			$fee_subtotal = $order_fee->get_total();
-		} else {
-			$fee_subtotal = ( $order_fee->get_total() + $order_fee->get_total_tax() );
-		}
-		$fee_price = number_format( $fee_subtotal, wc_get_price_decimals(), '.', '' ) * 100;
+		$fee_subtotal = ( $order_fee->get_total() + $order_fee->get_total_tax() );
+		$fee_price    = number_format( $fee_subtotal, wc_get_price_decimals(), '.', '' );
 		return round( $fee_price );
 	}
 
@@ -291,15 +258,21 @@ class Qliro_One_Request_Order {
 	 * @return int
 	 */
 	public function get_fee_total_tax_amount( $order, $order_fee ) {
-		if ( $this->separate_sales_tax ) {
-			$fee_tax_amount = 0;
-		} else {
-			$fee_total_amount       = $this->get_fee_total_amount( $order, $order_fee );
-			$fee_tax_rate           = ( '0' !== $order->get_total_tax() ) ? $this->get_order_line_tax_rate( $order, current( $order->get_items( 'fee' ) ) ) : 0;
-			$fee_total_exluding_tax = $fee_total_amount / ( 1 + ( $fee_tax_rate / 10000 ) );
-			$fee_tax_amount         = $fee_total_amount - $fee_total_exluding_tax;
-		}
-		$this->total_tax += round( $fee_tax_amount );
+		$fee_total_amount        = $this->get_fee_total_amount( $order, $order_fee );
+		$fee_tax_rate            = ( '0' !== $order->get_total_tax() ) ? $this->get_order_line_tax_rate( $order, current( $order->get_items( 'fee' ) ) ) : 0;
+		$fee_total_excluding_tax = $fee_total_amount / ( 1 + ( $fee_tax_rate ) );
+		$fee_tax_amount          = $fee_total_amount - $fee_total_excluding_tax;
+		$this->total_tax        += round( $fee_tax_amount );
 		return round( $fee_tax_amount );
+	}
+
+	/**
+	 * Generates request id.
+	 *
+	 * @return string
+	 * @throws Exception If an appropriate source of randomness cannot be found.
+	 */
+	public function generate_request_id() {
+		return sprintf( '%04X%04X-%04X-%04X-%04X-%04X%04X%04X', random_int( 0, 65535 ), random_int( 0, 65535 ), random_int( 0, 65535 ), random_int( 16384, 20479 ), random_int( 32768, 49151 ), random_int( 0, 65535 ), random_int( 0, 65535 ), random_int( 0, 65535 ) );
 	}
 }
