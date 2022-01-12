8c8ea5a41ce2fe2209447dd39e9e1bbd1e961f39
Miodrag Stojanovic <mio.sto@yandex.com>
2022-01-12 21:09:37 +0100

    include new helper file

--- qliro-one-for-woocommerce.php
+++ qliro-one-for-woocommerce.php
@@ -193,6 +193,7 @@ if ( ! class_exists( 'Qliro_One_For_WooCommerce' ) ) {
 			include_once QLIRO_WC_PLUGIN_PATH . '/classes/requests/helpers/class-qliro-one-merchant-urls.php';
 			include_once QLIRO_WC_PLUGIN_PATH . '/classes/requests/helpers/class-qliro-one-helper-cart.php';
 			include_once QLIRO_WC_PLUGIN_PATH . '/classes/requests/helpers/class-qliro-one-request-order.php';
+			include_once QLIRO_WC_PLUGIN_PATH . '/classes/requests/helpers/class-qliro-one-return-items-helper.php';
 			include_once QLIRO_WC_PLUGIN_PATH . '/classes/class-qliro-one-order-management.php';
 
 			$this->api              = new Qliro_One_API();
