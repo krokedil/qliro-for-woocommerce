c61bb72203da61d0187d02ed47f8b654984a0853
MichaelBengtsson <michael@krokedil.se>
2023-02-13 10:46:41 +0100

    Fix stripping html snippet from log

--- classes/class-qliro-one-logger.php
+++ classes/class-qliro-one-logger.php
@@ -57,19 +57,19 @@ class Qliro_One_Logger {
 	/**
 	 * Formats the log data to be logged.
 	 *
-	 * @param string $qliro_order_id The Qliro One order id.
-	 * @param string $method The method.
-	 * @param string $title The title for the log.
-	 * @param array  $request_args The request args.
-	 * @param array  $response The response.
-	 * @param string $code The status code.
-	 * @param string $request_url The request url.
+	 * @param string          $qliro_order_id The Qliro One order id.
+	 * @param string          $method The method.
+	 * @param string          $title The title for the log.
+	 * @param array           $request_args The request args.
+	 * @param object|WP_Error $response The response.
+	 * @param string          $code The status code.
+	 * @param string          $request_url The request url.
 	 * @return array
 	 */
 	public static function format_log( $qliro_order_id, $method, $title, $request_args, $response, $code, $request_url = null ) {
 		// Unset the snippet to prevent issues in the response.
-		if ( ! is_wp_error( $response ) && isset( $response['OrderHtmlSnippet'] ) ) {// todo check snippet.
-			unset( $response['OrderHtmlSnippet'] );
+		if ( ! is_wp_error( $response ) ) {
+			$response = self::remove_html_snippet( $response );
 		}
 		// Unset the snippet to prevent issues in the request body.
 		if ( isset( $request_args['body'] ) ) {
@@ -137,4 +137,20 @@ class Qliro_One_Logger {
 		update_option( 'krokedil_debuglog_qliro_one', $logs );
 	}
 
+	/**
+	 * Removes the HTML snippet from the response body if its set.
+	 * @param object $response
+	 * @return object
+	 */
+	public static function remove_html_snippet( $response ) {
+		$body = json_decode( wp_remote_retrieve_body( $response ), true );
+
+		if( isset( $body['OrderHtmlSnippet'] ) ) {
+			unset( $body['OrderHtmlSnippet'] );
+		}
+
+		$response['body'] = $body;
+
+		return $response;
+	}
 }
