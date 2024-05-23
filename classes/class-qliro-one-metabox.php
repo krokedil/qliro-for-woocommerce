0a9ec0df1d7461f3a6ee01481bd3ba794637b6bf
MichaelBengtsson <michael@krokedil.se>
2024-05-23 09:19:26 +0200

    Add order sync button to the metabox

--- classes/class-qliro-one-metabox.php
+++ classes/class-qliro-one-metabox.php
@@ -18,6 +18,8 @@ class Qliro_One_Metabox extends OrderMetabox {
 	 */
 	public function __construct() {
 		parent::__construct( 'qliro-one', __( 'Qliro', 'qliro-one' ), 'qliro_one' );
+
+		add_action( 'init', array( $this, 'handle_sync_order_action' ), 9999 );
 	}
 
 	/**
@@ -40,8 +42,10 @@ class Qliro_One_Metabox extends OrderMetabox {
 			return;
 		}
 
-		$qliro_order_id  = $order->get_meta( '_qliro_one_order_id' );
-		$qliro_reference = $order->get_meta( '_qliro_one_merchant_reference' );
+		$qliro_order_id       = $order->get_meta( '_qliro_one_order_id' );
+		$qliro_reference      = $order->get_meta( '_qliro_one_merchant_reference' );
+		$qliro_payment_method = $order->get_meta( 'qliro_one_payment_method_name' );
+		$is_captured          = $order->get_meta( '_qliro_order_captured' );
 
 		$qliro_order = QOC_WC()->api->get_qliro_one_admin_order( $qliro_order_id );
 
@@ -57,6 +61,68 @@ class Qliro_One_Metabox extends OrderMetabox {
 		self::output_info( __( 'Qliro reference', 'qliro-one' ), $qliro_reference );
 		self::output_info( __( 'Qliro order status', 'qliro-one' ), self::get_order_status( $last_transaction ) );
 		self::output_info( __( 'Amount', 'qliro-one' ), self::get_amount( $last_transaction ) );
+		self::output_sync_order_button( $order, $qliro_order_id );
+	}
+
+	/**
+	 * Handle the sync order action request.
+	 *
+	 * @return void
+	 */
+	public function handle_sync_order_action() {
+		if ( ! current_user_can( 'manage_woocommerce' ) ) {
+			return;
+		}
+
+		$nonce          = filter_input( INPUT_GET, '_wpnonce', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
+		$action         = filter_input( INPUT_GET, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
+		$order_id       = filter_input( INPUT_GET, 'order_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
+		$qliro_order_id = filter_input( INPUT_GET, 'qliro_order_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
+
+		if ( empty( $action ) || empty( $order_id ) ) {
+			return;
+		}
+
+		if ( 'qliro_one_sync_order' !== $action ) {
+			return;
+		}
+
+		if ( ! wp_verify_nonce( $nonce, 'qliro_one_sync_order' ) ) {
+			wp_safe_redirect( wp_get_referer() ? wp_get_referer() : admin_url( 'edit.php?post_type=shop_order' ) );
+			exit;
+		}
+
+		$order = wc_get_order( $order_id );
+
+		if ( ! $order || $this->payment_method_id !== $order->get_payment_method() ) {
+			return;
+		}
+
+		$response = QOC_WC()->api->om_update_qliro_one_order( $qliro_order_id, $order_id );
+
+		if ( is_wp_error( $response ) ) {
+			$order->add_order_note(
+				sprintf(
+					/* translators: %s: error message */
+					__( 'Failed to sync order with Qliro. Error: %s', 'qliro-one' ),
+					$response->get_error_message()
+				)
+			);
+			return;
+		}
+
+		// Get the new payment transaction id from the response, and update the order meta with it.
+		$transaction_id = $response['PaymentTransactions'][0]['PaymentTransactionId'] ?? '';
+		$order->update_meta_data( '_qliro_payment_transaction_id', $transaction_id );
+		$order->save();
+
+		$order->add_order_note(
+			// translators: %s: new transaction id from Qliro.
+			sprintf( __( 'Order synced with Qliro. Transaction ID: %s', 'qliro-one' ), $transaction_id )
+		);
+
+		wp_safe_redirect( wp_get_referer() ? wp_get_referer() : admin_url( 'edit.php?post_type=shop_order' ) );
+		exit;
 	}
 
 	/**
@@ -133,5 +199,47 @@ class Qliro_One_Metabox extends OrderMetabox {
 
 		// Return the method but ensure only the first letter is uppercase.
 		return ucfirst( strtolower( $payment_method ) ) . wc_help_tip( $subtype );
+	}
+
+	/**
+	 * Output the sync order action button.
+	 *
+	 * @param WC_Order $order The WooCommerce order.
+	 * @param string   $qliro_order_id The Qliro order id.
+	 *
+	 * @return void
+	 */
+	private static function output_sync_order_button( $order, $qliro_order_id ) {
+		$is_captured    = $order->get_meta( '_qliro_order_captured' );
+		$is_cancelled   = $order->get_meta( '_qliro_order_cancelled' );
+		$payment_method = $order->get_meta( 'qliro_one_payment_method_name' );
+
+		// Only output the sync button if the order is a Qliro payment method order. Cant update card orders for example.
+		if ( strpos( $payment_method, 'QLIRO_' ) !== 0 ) {
+			return;
+		}
+
+		// If the order is captured or cancelled, do not output the sync button.
+		if ( $is_captured || $is_cancelled ) {
+			return;
+		}
+
+		$query_args = array(
+			'action'         => 'qliro_one_sync_order',
+			'order_id'       => $order->get_id(),
+			'qliro_order_id' => $qliro_order_id,
+		);
+
+		$action_url = wp_nonce_url(
+			add_query_arg( $query_args, admin_url( 'admin-ajax.php' ) ),
+			'qliro_one_sync_order'
+		);
+
+		self::output_action_button(
+			__( 'Sync order with Qliro', 'qliro-one' ),
+			$action_url,
+			false,
+			'button-primary'
+		);
 	}
 }
