f0bef33d9b2fbeb8e9f529e3571fa8e59b169121
MichaelBengtsson <michael@krokedil.se>
2024-12-13 11:09:25 +0100

    Update support for the gateway to include subscriptions and tokenization

--- classes/class-qliro-one-gateway.php
+++ classes/class-qliro-one-gateway.php
@@ -44,6 +44,17 @@ class Qliro_One_Gateway extends WC_Payment_Gateway {
 				'products',
 				'refunds',
 				'upsell',
+				'subscriptions',
+				'subscription_cancellation',
+				'subscription_suspension',
+				'subscription_reactivation',
+				'subscription_amount_changes',
+				'subscription_date_changes',
+				'subscription_payment_method_change',
+				'subscription_payment_method_change_customer',
+				'subscription_payment_method_change_admin',
+				'multiple_subscriptions',
+				'tokenization', // Only for card payments when buying subscriptions.
 			)
 		);
 		$this->has_fields         = false;
