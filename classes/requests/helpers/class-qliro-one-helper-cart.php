91f0f5f40f36266eab0e152b43fff7e9a3480423
moory-se <95882730+moory-se@users.noreply.github.com>
2025-03-26 09:44:34 +0100

    Use MD5 to ensure valid reference

--- classes/requests/helpers/class-qliro-one-helper-cart.php
+++ classes/requests/helpers/class-qliro-one-helper-cart.php
@@ -179,7 +179,10 @@ class Qliro_One_Helper_Cart {
 	 * @return array
 	 */
 	public static function get_fee( $fee ) {
-		$reference = sanitize_title_with_dashes( $fee->name );
+        //We have no real identifier from WooCommerce that can be reliably used. The name
+        //might contain forbidden characters and might exceed the max-length from Qliro.
+        //By using MD5 we make sure that we always create strings that pass the regex.
+		$reference = md5( sanitize_title_with_dashes( $fee->name ) );
 
 		return array(
 			'MerchantReference'  => $reference,
