fb589785b7437962cb3787874fa6f7ac6ed409ed
Montazar <github.v0its@slmail.me>
2025-05-20 11:57:27 +0200

    Version 1.11.0

--- qliro-one-for-woocommerce.php
+++ qliro-one-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: Qliro Checkout payment gateway for WooCommerce.
  * Author: Krokedil
  * Author URI: https://krokedil.com/
- * Version: 1.10.1
+ * Version: 1.11.0
  * Text Domain: qliro-one-for-woocommerce
  * Domain Path: /languages
  *
@@ -40,7 +40,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Required minimums and constants
  */
-define( 'QLIRO_WC_VERSION', '1.10.1' );
+define( 'QLIRO_WC_VERSION', '1.11.0' );
 define( 'QLIRO_WC_MAIN_FILE', __FILE__ );
 define( 'QLIRO_WC_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'QLIRO_WC_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
