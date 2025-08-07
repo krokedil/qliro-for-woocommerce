5507bb19bcee658aa9197d9811e1093b4ed120e8
Montazar <github.v0its@slmail.me>
2025-08-07 16:18:51 +0200

    PHPCS formatting

--- includes/admin/views/html-order-add-discount.php
+++ includes/admin/views/html-order-add-discount.php
@@ -26,85 +26,85 @@ $section_1 = array(
 		'name' => '',
 		'type' => 'title',
 	),
-	'discount_id' => array(
-		'name' => 'Rabatt-ID',
+	'discount_id'   => array(
+		'name'     => 'Rabatt-ID',
 		'desc_tip' => true,
-		'desc' => 'Innehåller artikelnummer och rabattnummer. Ex. artikelnummer_rabatt01',
-		'id' => 'qliro-discount-id',
-		'type' => 'text',
+		'desc'     => 'Innehåller artikelnummer och rabattnummer. Ex. artikelnummer_rabatt01',
+		'id'       => 'qliro-discount-id',
+		'type'     => 'text',
 	),
-	'section_end' => array(
+	'section_end'   => array(
 		'type' => 'sectionend',
-	)
+	),
 );
 
 $section_2 = array(
-	'section_title' => array(
+	'section_title'       => array(
 		'name' => 'Fyll i SEK eller procent',
 		'type' => 'title',
 	),
-	'discount_amount' => array(
-		'name' => 'Totalbelopp (SEK)',
-		'id' => 'qliro-discount-amount',
-		'type' => 'number',
-		'placeholder' => 'SEK',
+	'discount_amount'     => array(
+		'name'              => 'Totalbelopp (SEK)',
+		'id'                => 'qliro-discount-amount',
+		'type'              => 'number',
+		'placeholder'       => 'SEK',
 		'custom_attributes' => array(
 			'step' => 'any',
-			'min' => '0.00',
-			'max' => $total_amount,
+			'min'  => '0.00',
+			'max'  => $total_amount,
 		),
 	),
 	'discount_percentage' => array(
-		'name' => 'Procent (%)',
-		'id' => 'qliro-discount-percentage',
-		'type' => 'number',
-		'placeholder' => '%',
+		'name'              => 'Procent (%)',
+		'id'                => 'qliro-discount-percentage',
+		'type'              => 'number',
+		'placeholder'       => '%',
 		'custom_attributes' => array(
 			'step' => 'any',
-			'min' => '0.00',
-			'max' => '100.00',
+			'min'  => '0.00',
+			'max'  => '100.00',
 		),
 	),
-	'section_end' => array(
+	'section_end'         => array(
 		'type' => 'sectionend',
-	)
+	),
 );
 
 $section_3 = array(
-	'section_title' => array(
+	'section_title'           => array(
 		'name' => 'Nytt belopp att betala',
 		'type' => 'title',
 	),
-	'total_amount' => array(
-		'name' => 'Totalbelopp innan',
-		'id' => 'qliro-total-amount',
-		'type' => 'text',
-		'value' => "$total_amount SEK",
+	'total_amount'            => array(
+		'name'              => 'Totalbelopp innan',
+		'id'                => 'qliro-total-amount',
+		'type'              => 'text',
+		'value'             => "$total_amount SEK",
 		'custom_attributes' => array(
 			'readonly' => 'readonly',
 		),
 	),
 	'new_discount_percentage' => array(
-		'name' => 'Rabatt',
-		'id' => 'qliro-new-discount-percentage',
-		'type' => 'text',
-		'value' => '0.00',
+		'name'              => 'Rabatt',
+		'id'                => 'qliro-new-discount-percentage',
+		'type'              => 'text',
+		'value'             => '0.00',
 		'custom_attributes' => array(
 			'readonly' => 'readonly',
 		),
 	),
-	'new_total_amount' => array(
-		'name' => 'Nytt belopp att betala',
-		'id' => 'qliro-new-total-amount',
-		'type' => 'text',
-		'value' => "$total_amount SEK",
+	'new_total_amount'        => array(
+		'name'              => 'Nytt belopp att betala',
+		'id'                => 'qliro-new-total-amount',
+		'type'              => 'text',
+		'value'             => "$total_amount SEK",
 		'custom_attributes' => array(
 			'readonly' => 'readonly',
 		),
 	),
-	'section_end' => array(
+	'section_end'             => array(
 		'type' => 'sectionend',
-	)
+	),
 );
 ?>
 
@@ -118,17 +118,17 @@ $section_3 = array(
 						<span class="screen-reader-text">Close modal panel</span>
 					</button>
 				</header>
-				<article id="qliro-discount-form" style="max-height: 851.25px;" data-fees="<?php echo esc_attr($fees); ?>" data-total-amount="<?php echo esc_attr( $total_amount ); ?>">
-					<?php woocommerce_admin_fields($section_1); ?>
+				<article id="qliro-discount-form" style="max-height: 851.25px;" data-fees="<?php echo esc_attr( $fees ); ?>" data-total-amount="<?php echo esc_attr( $total_amount ); ?>">
+					<?php woocommerce_admin_fields( $section_1 ); ?>
 					<p id="qliro-discount-id-error" class="explanation hidden error">Rabatt-ID måste vara unikt</p>
 					<hr>
 
-					<?php woocommerce_admin_fields($section_2); ?>
+					<?php woocommerce_admin_fields( $section_2 ); ?>
 					<p id="qliro-discount-notice" class="explanation">Procentsatsen är baserad på totalbelopp</p>
 					<p id="qliro-discount-error" class="woocommerce-error explanation error hidden">Beloppet får inte vara lika med eller överstiga totalbelopp.</p>
 					<hr>
 
-					<?php woocommerce_admin_fields($section_3); ?>
+					<?php woocommerce_admin_fields( $section_3 ); ?>
 
 				</article>
 				<footer>
