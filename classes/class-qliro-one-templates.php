b11ebb8f79ab46688d4901b1fb399c003c1daba8
MichaelBengtsson <michael@krokedil.se>
2025-09-15 13:20:45 +0200

    Fix deprecated notice when passing a function call directly to reset function

--- classes/class-qliro-one-templates.php
+++ classes/class-qliro-one-templates.php
@@ -91,7 +91,8 @@ class Qliro_One_Templates {
 			return;
 		}
 
-		$first_gateway = reset( WC()->payment_gateways()->get_available_payment_gateways() );
+		$available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
+		$first_gateway = reset( $available_gateways );
 		if ( ! isset( WC()->session ) || ( 'qliro_one' !== WC()->session->get( 'chosen_payment_method' ) && 'qliro_one' !== $first_gateway->id ) ) {
 			return;
 		}
