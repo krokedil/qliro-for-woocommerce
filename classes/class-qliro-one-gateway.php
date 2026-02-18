76ab352bc5847925af1846016ab84769473a6136
Montazar <github.mdtk4@passmail.com>
2026-02-18 23:48:01 +0100

    Throw in process_payment to signal failure

--- classes/class-qliro-one-gateway.php
+++ classes/class-qliro-one-gateway.php
@@ -132,11 +132,9 @@ class Qliro_One_Gateway extends WC_Payment_Gateway {
 				$result = QLIRO_WC()->api->create_qliro_one_order( $order_id );
 
 				if ( is_wp_error( $result ) ) {
-					return array(
-						'result'   => 'failure',
-						'messages' => $result->get_error_message(),
-					);
+					throw new Exception( $result->get_error_message() );
 				}
+
 				$payment_link = $result['PaymentLink'] ?? '';
 				$order->update_meta_data( '_qliro_one_order_id', $result['OrderId'] );
 				$order->update_meta_data( '_qliro_one_merchant_reference', $order->get_order_number() );
@@ -149,10 +147,7 @@ class Qliro_One_Gateway extends WC_Payment_Gateway {
 			}
 
 			if ( empty( $redirect_url ) ) {
-				return array(
-					'result'   => 'failure',
-					'messages' => __( 'Could not retrieve the Qliro payment link. Please contact the store administrator.', 'qliro-for-woocommerce' ),
-				);
+				throw new Exception( __( 'Could not retrieve the Qliro payment link. Please contact the store administrator.', 'qliro-for-woocommerce' ) );
 			}
 
 			return array(
@@ -170,10 +165,7 @@ class Qliro_One_Gateway extends WC_Payment_Gateway {
 		// If the order id, confirmation id or merchant reference is not set, we can not proceed.
 		if ( empty( $qliro_order_id ) || empty( $qliro_confirmation_id ) || empty( $qliro_merchant_reference ) ) {
 			Qliro_One_Logger::log( "Could not process payment due to missing session data. qliro_one_order_id: $qliro_order_id, qliro_order_confirmation_id: $qliro_confirmation_id, qliro_one_merchant_reference: $qliro_merchant_reference" );
-			return array(
-				'result'   => 'failure',
-				'messages' => __( 'The order could not be processed. Please reload the page and try again.', 'qliro-for-woocommerce' ),
-			);
+			throw new Exception( __( 'The order could not be processed. Please reload the page and try again.', 'qliro-for-woocommerce' ) );
 		}
 
 		$order->update_meta_data( '_qliro_one_order_id', $qliro_order_id );
