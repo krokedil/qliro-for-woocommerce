203096f5d973515d18c14d716416def7c591a091
anyakro <anya@krokedil.se>
2026-08-26 07:24:45 +0200

    Improve handling of illegal SKU:s

--- classes/requests/class-qliro-one-request.php
+++ classes/requests/class-qliro-one-request.php
@@ -203,6 +203,9 @@ abstract class Qliro_One_Request {
 					$message = __( 'Qliro is not configured for the selected country and currency. Please select a different payment method or country.', 'qliro-for-woocommerce' );
 				}
 				break;
+			case 'INVALID_INPUT':
+				$message = __( 'Qliro is unfortunately not available for this order. Please choose another payment method, or contact us if the problem continues. Error code INVALID_INPUT', 'qliro-for-woocommerce' );
+				break;
 			case 'NO_ITEMS_LEFT_IN_RESERVATION':
 				$message = __( 'The order has already been captured.', 'qliro-for-woocommerce' );
 				break;
